95ec72b8d0b8dc812184b2bb5423f9aaa979a0ce
Kawabe <b1012067@fun.ac.jp>
2014-10-31 13:34:45 +0900

    健康ウォーキングマップに遷移するようにした close #8

--- tourism-app/CourseDetailViewController.m
+++ tourism-app/CourseDetailViewController.m
@@ -53,7 +53,7 @@ Course *course;
  @return セクションに含まれるCellの数
  */
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 9 + [course.spot_name count];
+    return 8 + [course.spot_name count];
 }
 
 /**
@@ -69,6 +69,12 @@ Course *course;
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
     }
     
+    //UITableViewのCellの値がスクロールするごとに重なったり壊れるなどの問題を防ぐために
+    //最初に全てのcell.accessoryTypeをNoneに設定する
+    cell.accessoryType = UITableViewCellAccessoryNone;
+    //セルを押したとき青くならなくする
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+
     //UITableViewのCellの値がスクロールするごとに重なったり壊れる,UITableViewでCell再描画時に文字が重なる
     //などの問題を防ぐために、CellのsubViewを消去する
     for (UIView *subview in [cell.contentView subviews]) {
@@ -80,7 +86,7 @@ Course *course;
         //cell.textLabel.textに空文字を代入する
         cell.textLabel.text = @"";
         
-        UIButton * walkUIButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        UIButton *walkUIButton = [UIButton buttonWithType:UIButtonTypeCustom];
         [walkUIButton setFrame:CGRectMake(0, 0, 213.0, 37.0)];
         [walkUIButton setCenter:CGPointMake(self.view.frame.size.width/2, 70.0/2.0)];
         [walkUIButton setImage:[UIImage imageNamed:@"walk_course.png"] forState:UIControlStateNormal];
@@ -96,7 +102,7 @@ Course *course;
         //cell.textLabel.textに空文字を代入する
         cell.textLabel.text = @"";
 
-        UIButton * walkingmapUIButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        UIButton *walkingmapUIButton = [UIButton buttonWithType:UIButtonTypeCustom];
         [walkingmapUIButton setFrame:CGRectMake(0, 0, 213.0, 37.0)];
         [walkingmapUIButton setCenter:CGPointMake(self.view.frame.size.width/2, 70.0/2.0)];
         [walkingmapUIButton setImage:[UIImage imageNamed:@"see_walkingmap.png"] forState:UIControlStateNormal];
@@ -119,11 +125,9 @@ Course *course;
     }else if(indexPath.row == 6){
         cell.textLabel.text = @"タグ画像を入れる";
     }else if(indexPath.row == 7 + [course.spot_name count]){
-        cell.textLabel.text = @"歩いた記録を残す";
-        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-    }else if(indexPath.row == 8 + [course.spot_name count]){
         cell.textLabel.text = @"Powered by 健康ウォーキングマップ";
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        cell.selectionStyle = UITableViewCellSelectionStyleDefault;
     }
     
     for(int i = 0;i < [course.spot_name count];i++){
@@ -136,14 +140,27 @@ Course *course;
 }
 
 /**
- Cellに入れたボタンが押された際に、呼び出される
+ セルタップ時に呼び出される
+ */
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    //ハイライトを外す
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    if(indexPath.row == 7 + [course.spot_name count]){
+        NSURL *url = [NSURL URLWithString:@"http://www.city.hakodate.hokkaido.jp/docs/2014012700900/"];
+        [[UIApplication sharedApplication] openURL:url];
+    }
+}
+
+/**
+ Cellに入れたボタン(このコースを歩く)が押された際に、呼び出される
  */
 - (void)walkUIButtonDidPush:(UIButton*)sender {
     [self performSegueWithIdentifier:@"detailmap" sender:self];
 }
 
 /**
- Cellに入れたボタンが押された際に、呼び出される
+ Cellに入れたボタン(健康ウォーキングマップを見る)が押された際に、呼び出される
  */
 - (void)walkingmapUIButtonDidPush:(UIButton*)sender {
     [self performSegueWithIdentifier:@"walkingmap" sender:self];
