d289dc0b408139f7ef9e592a4232d09b5b9aee55
Kawabe <b1012067@fun.ac.jp>
2014-11-21 22:34:09 +0900

    コース詳細画面にセクションを追加し、カテゴリとスポットを上に移動させた close #111

--- tourism-app/CourseDetailViewController.m
+++ tourism-app/CourseDetailViewController.m
@@ -48,12 +48,51 @@ Course *course;
 }
 
 /**
+ @return セクションの数
+ */
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 5;
+}
+
+/**
  ロード時に呼び出される
  
  @return セクションに含まれるCellの数
  */
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 8 + [course.spot_name count];
+    //return 8 + [course.spot_name count];
+    if(section == 0){
+        return 2;
+    }else if (section == 1){
+        return 1;
+    }else if (section == 2){
+        return [course.spot_name count];
+    }else if(section == 3){
+        return 4;
+    }else if (section == 4){
+        return 1;
+    }
+    
+    return 0;
+}
+
+/**
+ @return セクションのタイトル
+ */
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    if(section == 0){
+        return @"コース詳細";
+    }else if(section == 1){
+        return @"カテゴリ";
+    }else if (section == 2){
+        return @"スポット";
+    }else if(section == 3){
+        return @"コースの特徴";
+    }else if (section == 4){
+        return @"出典";
+    }
+    
+    return nil;
 }
 
 /**
@@ -76,125 +115,113 @@ Course *course;
     //最初に全てのcell.imageView.imageをnilに設定する
     cell.imageView.image = nil;
     
-    if(indexPath.row == 0){
-        //UITableViewのCellの値がスクロールするごとに重なったり壊れる,UITableViewでCell再描画時に文字が重なる問題を防ぐために、
-        //cell.textLabel.textに空文字を代入する
-        cell.textLabel.text = @"";
-        
-        UIButton *walkUIButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [walkUIButton setFrame:CGRectMake(0, 0, 213.0, 37.0)];
-        [walkUIButton setCenter:CGPointMake(self.view.frame.size.width/2, 70.0/2.0)];
-        [walkUIButton setImage:[UIImage imageNamed:@"walk_course.png"] forState:UIControlStateNormal];
-        [walkUIButton setImage:[UIImage imageNamed:@"walk_course.png"] forState:UIControlStateSelected];
-        [walkUIButton addTarget:self
-                         action:@selector(walkUIButtonDidPush:)
-               forControlEvents:UIControlEventTouchUpInside];
-        
-        [cell setSelectionStyle: (UITableViewCellSelectionStyle)UITableViewCellEditingStyleNone];
-        [cell.contentView addSubview:walkUIButton];
-    }else if(indexPath.row == 1){
-        //UITableViewのCellの値がスクロールするごとに重なったり壊れる,UITableViewでCell再描画時に文字が重なる問題を防ぐために、
-        //cell.textLabel.textに空文字を代入する
-        cell.textLabel.text = @"";
-        
-        UIButton *walkingmapUIButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        [walkingmapUIButton setFrame:CGRectMake(0, 0, 213.0, 37.0)];
-        [walkingmapUIButton setCenter:CGPointMake(self.view.frame.size.width/2, 70.0/2.0)];
-        [walkingmapUIButton setImage:[UIImage imageNamed:@"see_walkingmap.png"] forState:UIControlStateNormal];
-        [walkingmapUIButton setImage:[UIImage imageNamed:@"see_walkingmap.png"] forState:UIControlStateSelected];
-        [walkingmapUIButton addTarget:self
-                               action:@selector(walkingmapUIButtonDidPush:)
-                     forControlEvents:UIControlEventTouchUpInside];
-        
-        [cell setSelectionStyle: (UITableViewCellSelectionStyle)UITableViewCellEditingStyleNone];
-        [cell.contentView addSubview:walkingmapUIButton];
-    }else if(indexPath.row == 2){
-        cell.textLabel.text = [NSString stringWithFormat:@"所要時間:%d分", course.time];
-    }else if(indexPath.row == 3){
-        cell.textLabel.text = [NSString stringWithFormat:@"移動距離:%.1fkm", course.distance];
-    }else if(indexPath.row == 4){
-        cell.textLabel.text = [NSString stringWithFormat:@"男性消費カロリー:%dkcal\n女性消費カロリー:%dkcal", course.male_calories, course.female_calories];
-        cell.textLabel.numberOfLines = 0; //改行可
-    }else if(indexPath.row == 5){
-        cell.textLabel.text = [NSString stringWithFormat:@"歩数:%d歩", course.steps];
-    }else if(indexPath.row == 6){
-        //タグアイコンの表示
-        UIImage *spring_image = [UIImage imageNamed:@"spring.png"];
-        UIImageView *spring_tag = [[UIImageView alloc]initWithImage:spring_image];
-        spring_tag.frame = CGRectMake(20, 20, 40, 40);
-        [cell.contentView addSubview:spring_tag];
-        
-        UIImage *summer_image = [UIImage imageNamed:@"summer.png"];
-        UIImageView *summer_tag = [[UIImageView alloc]initWithImage:summer_image];
-        summer_tag.frame = CGRectMake(65, 20, 40, 40);
-        [cell.contentView addSubview:summer_tag];
-        
-        UIImage *autumn_image = [UIImage imageNamed:@"autumn.png"];
-        UIImageView *autumn_tag = [[UIImageView alloc]initWithImage:autumn_image];
-        autumn_tag.frame = CGRectMake(110, 20, 40, 40);
-        [cell.contentView addSubview:autumn_tag];
-        
-        UIImage *winter_image = [UIImage imageNamed:@"winter.png"];
-        UIImageView *winter_tag = [[UIImageView alloc]initWithImage:winter_image];
-        winter_tag.frame = CGRectMake(155, 20, 40, 40);
-        [cell.contentView addSubview:winter_tag];
-        
-        UIImage *park_image = [UIImage imageNamed:@"park.png"];
-        UIImageView *park_tag = [[UIImageView alloc]initWithImage:park_image];
-        park_tag.frame = CGRectMake(200, 20, 40, 40);
-        [cell.contentView addSubview:park_tag];
-        
-        UIImage *sea_image = [UIImage imageNamed:@"sea.png"];
-        UIImageView *sea_tag = [[UIImageView alloc]initWithImage:sea_image];
-        sea_tag.frame = CGRectMake(245, 20, 40, 40);
-        [cell.contentView addSubview:sea_tag];
-        
-        
-        //タグアイコンの透明度の設定
-        if(![course.tag_name containsObject:@"春"]){
-            spring_tag.alpha = 0.2;
+    if(indexPath.section == 0){
+        if(indexPath.row == 0){
+            cell.textLabel.text = @"このコースを歩く";
+            cell.imageView.image = [UIImage imageNamed:@"map_button.png"];
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            cell.selectionStyle = UITableViewCellSelectionStyleDefault;
+        }else if(indexPath.row == 1){
+            cell.textLabel.text = @"健康ウォーキングマップを見る";
+            cell.textLabel.numberOfLines = 0;
+            cell.imageView.image = [UIImage imageNamed:@"walkingmap_button.png"];
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            cell.selectionStyle = UITableViewCellSelectionStyleDefault;
         }
-        if(![course.tag_name containsObject:@"夏"]){
-            summer_tag.alpha = 0.2;
-        }
-        if(![course.tag_name containsObject:@"秋"]){
-            autumn_tag.alpha = 0.2;
-        }
-        if(![course.tag_name containsObject:@"冬"]){
-            winter_tag.alpha = 0.2;
-        }
-        if(![course.tag_name containsObject:@"公園"]){
-            park_tag.alpha = 0.2;
-        }
-        if(![course.tag_name containsObject:@"海"]){
-            sea_tag.alpha = 0.2;
-        }
-    }else if(indexPath.row == 7 + [course.spot_name count]){
-        cell.textLabel.text = @"Powered by 健康ウォーキングマップ";
-        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        cell.selectionStyle = UITableViewCellSelectionStyleDefault;
-    }
-    
-    for(int i = 0;i < [course.spot_name count];i++){
-        if(indexPath.row == 7 + i){
-            //コース名をCellのViewに追加する
-            if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone){
-                NSLog(@"iPhoneの処理");
-                UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake(cell.textLabel.frame.origin.x + 130, cell.textLabel.frame.origin.y - 20, cell.frame.size.width - 50, 120)];
-                textLabel.text = [course.spot_name objectAtIndex:i];
-                [cell.contentView addSubview:textLabel];
-            }else{
-                NSLog(@"iPadの処理");
-                UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake(cell.textLabel.frame.origin.x + 130, cell.textLabel.frame.origin.y - 20, cell.frame.size.width + 100, 120)];
-                textLabel.text = [course.spot_name objectAtIndex:i];
-                [cell.contentView addSubview:textLabel];
-            }
+    }else if(indexPath.section == 1){
+        if(indexPath.row == 0){
+            //タグアイコンの表示
+            UIImage *spring_image = [UIImage imageNamed:@"spring.png"];
+            UIImageView *spring_tag = [[UIImageView alloc]initWithImage:spring_image];
+            spring_tag.frame = CGRectMake(20, 20, 40, 40);
+            [cell.contentView addSubview:spring_tag];
             
-            //スポットの画像をCellのViewに追加する
-            UIImage *image = [UIImage imageNamed:[course.spot_image_name objectAtIndex:i]];
-            UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
-            imageView.frame = CGRectMake(10, 0, 110.0f, 79.0f);
-            [cell.contentView addSubview:imageView];
+            UIImage *summer_image = [UIImage imageNamed:@"summer.png"];
+            UIImageView *summer_tag = [[UIImageView alloc]initWithImage:summer_image];
+            summer_tag.frame = CGRectMake(65, 20, 40, 40);
+            [cell.contentView addSubview:summer_tag];
+            
+            UIImage *autumn_image = [UIImage imageNamed:@"autumn.png"];
+            UIImageView *autumn_tag = [[UIImageView alloc]initWithImage:autumn_image];
+            autumn_tag.frame = CGRectMake(110, 20, 40, 40);
+            [cell.contentView addSubview:autumn_tag];
+            
+            UIImage *winter_image = [UIImage imageNamed:@"winter.png"];
+            UIImageView *winter_tag = [[UIImageView alloc]initWithImage:winter_image];
+            winter_tag.frame = CGRectMake(155, 20, 40, 40);
+            [cell.contentView addSubview:winter_tag];
+            
+            UIImage *park_image = [UIImage imageNamed:@"park.png"];
+            UIImageView *park_tag = [[UIImageView alloc]initWithImage:park_image];
+            park_tag.frame = CGRectMake(200, 20, 40, 40);
+            [cell.contentView addSubview:park_tag];
+            
+            UIImage *sea_image = [UIImage imageNamed:@"sea.png"];
+            UIImageView *sea_tag = [[UIImageView alloc]initWithImage:sea_image];
+            sea_tag.frame = CGRectMake(245, 20, 40, 40);
+            [cell.contentView addSubview:sea_tag];
+            
+            
+            //タグアイコンの透明度の設定
+            if(![course.tag_name containsObject:@"春"]){
+                spring_tag.alpha = 0.2;
+            }
+            if(![course.tag_name containsObject:@"夏"]){
+                summer_tag.alpha = 0.2;
+            }
+            if(![course.tag_name containsObject:@"秋"]){
+                autumn_tag.alpha = 0.2;
+            }
+            if(![course.tag_name containsObject:@"冬"]){
+                winter_tag.alpha = 0.2;
+            }
+            if(![course.tag_name containsObject:@"公園"]){
+                park_tag.alpha = 0.2;
+            }
+            if(![course.tag_name containsObject:@"海"]){
+                sea_tag.alpha = 0.2;
+            }
+        }
+    }else if(indexPath.section == 2){
+        for(int i = 0;i < [course.spot_name count];i++){
+            if(indexPath.row == i){
+                //コース名をCellのViewに追加する
+                if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone){
+                    NSLog(@"iPhoneの処理");
+                    UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake(cell.textLabel.frame.origin.x + 130, cell.textLabel.frame.origin.y - 20, cell.frame.size.width - 50, 120)];
+                    textLabel.text = [course.spot_name objectAtIndex:i];
+                    [cell.contentView addSubview:textLabel];
+                }else{
+                    NSLog(@"iPadの処理");
+                    UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake(cell.textLabel.frame.origin.x + 130, cell.textLabel.frame.origin.y - 20, cell.frame.size.width + 100, 120)];
+                    textLabel.text = [course.spot_name objectAtIndex:i];
+                    [cell.contentView addSubview:textLabel];
+                }
+                
+                //スポットの画像をCellのViewに追加する
+                UIImage *image = [UIImage imageNamed:[course.spot_image_name objectAtIndex:i]];
+                UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
+                imageView.frame = CGRectMake(10, 0, 110.0f, 79.0f);
+                [cell.contentView addSubview:imageView];
+            }
+        }
+    }else if(indexPath.section == 3){
+        if(indexPath.row == 0){
+            cell.textLabel.text = [NSString stringWithFormat:@"所要時間:%d分", course.time];
+        }else if(indexPath.row == 1){
+            cell.textLabel.text = [NSString stringWithFormat:@"移動距離:%.1fkm", course.distance];
+        }else if(indexPath.row == 2){
+            cell.textLabel.text = [NSString stringWithFormat:@"男性消費カロリー:%dkcal\n女性消費カロリー:%dkcal", course.male_calories, course.female_calories];
+            cell.textLabel.numberOfLines = 0; //改行可
+        }else if(indexPath.row == 3){
+            cell.textLabel.text = [NSString stringWithFormat:@"歩数:%d歩", course.steps];
+        }
+    }else if(indexPath.section == 4){
+        if(indexPath.row == 0){
+            cell.textLabel.text = @"Powered by 健康ウォーキングマップ";
+            cell.textLabel.textColor = [UIColor blueColor];
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            cell.selectionStyle = UITableViewCellSelectionStyleDefault;
         }
     }
     
@@ -208,24 +235,14 @@ Course *course;
     //ハイライトを外す
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    if(indexPath.row == 7 + [course.spot_name count]){
+    if(indexPath.row == 0){
+        [self performSegueWithIdentifier:@"detailmap" sender:self];
+    }else if(indexPath.row == 1){
+        [self performSegueWithIdentifier:@"walkingmap" sender:self];
+    }else if(indexPath.row == 7 + [course.spot_name count]){
         NSURL *url = [NSURL URLWithString:@"http://www.city.hakodate.hokkaido.jp/docs/2014012700900/"];
         [[UIApplication sharedApplication] openURL:url];
     }
-}
-
-/**
- Cellに入れたボタン(このコースを歩く)が押された際に、呼び出される
- */
-- (void)walkUIButtonDidPush:(UIButton*)sender {
-    [self performSegueWithIdentifier:@"detailmap" sender:self];
-}
-
-/**
- Cellに入れたボタン(健康ウォーキングマップを見る)が押された際に、呼び出される
- */
-- (void)walkingmapUIButtonDidPush:(UIButton*)sender {
-    [self performSegueWithIdentifier:@"walkingmap" sender:self];
 }
 
 /**
